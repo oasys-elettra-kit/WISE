92f7f34aa64a276f09884b11c17696c50b07e7e1
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-10-30 17:48:31 +0100

    Bug Fixing

--- orangecontrib/wise/widgets/wofry/ow_wise_source_to_wofry.py
+++ orangecontrib/wise/widgets/wofry/ow_wise_source_to_wofry.py
@@ -1,20 +1,21 @@
 import numpy
-from oasys.widgets import gui as oasysgui
-
-from oasys.widgets import widget
 
 from PyQt5.QtWidgets import QApplication, QMessageBox
 from PyQt5.QtCore import QRect
 
 from orangewidget.settings import Setting
+from orangewidget import gui
+
+from oasys.widgets import gui as oasysgui
+from oasys.widgets.widget import AutomaticWidget
 
 from wofry.propagator.wavefront1D.generic_wavefront import GenericWavefront1D
+from wofrywise.propagator.wavefront1D.wise_wavefront import WiseWavefront
 
 from orangecontrib.wise.util.wise_objects import WiseOutput
 
-from wofrywise.propagator.wavefront1D.wise_wavefront import WiseWavefront
 
-class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
+class OWWiseSourceToWofryWavefront1d(AutomaticWidget):
     name = "Wise Source To Wofry Wavefront 1D"
     id = "toWofryWavefront1D"
     description = "Wise Source To Wofry Wavefront 1D"
@@ -30,29 +31,63 @@ class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
                 "doc":"GenericWavefront1D",
                 "id":"GenericWavefront1D"}]
 
-    CONTROL_AREA_WIDTH = 405
+    MAX_WIDTH = 420
+    MAX_HEIGHT = 200
+    CONTROL_AREA_WIDTH = 410
 
     want_main_area = 0
 
     number_of_points = Setting(500)
-
+    
+    source = None
+    
     def __init__(self):
         super().__init__()
 
         geom = QApplication.desktop().availableGeometry()
         self.setGeometry(QRect(round(geom.width()*0.05),
                                round(geom.height()*0.05),
-                               round(min(geom.width()*0.98, self.CONTROL_AREA_WIDTH+10)),
-                               round(min(geom.height()*0.95, 100))))
+                               round(min(geom.width()*0.98, self.MAX_WIDTH)),
+                               round(min(geom.height()*0.95, self.MAX_HEIGHT))))
 
+        self.setMinimumHeight(self.geometry().height())
+        self.setMinimumWidth(self.geometry().width())
         self.setMaximumHeight(self.geometry().height())
         self.setMaximumWidth(self.geometry().width())
 
-        self.controlArea.setFixedWidth(self.CONTROL_AREA_WIDTH)
+        self.controlArea.setFixedWidth(self.MAX_WIDTH-10)
+        self.controlArea.setFixedHeight(self.MAX_HEIGHT-10)
 
-        main_box = oasysgui.widgetBox(self.controlArea, "WISE Source to Wofry Wavefront Converter", orientation="vertical", width=self.CONTROL_AREA_WIDTH-5, height=50)
+
+        main_box = oasysgui.widgetBox(self.controlArea, "WISE Source to Wofry Wavefront Converter", orientation="vertical", width=self.CONTROL_AREA_WIDTH-5, height=110)
 
         oasysgui.lineEdit(main_box, self, "number_of_points", "Number of Points", labelWidth=260, valueType=float, orientation="horizontal")
+
+        gui.button(main_box, self, "Compute", height=40, callback=self.compute)
+
+    def compute(self):
+        if not self.source is None:
+            try:
+                self.source = self.input_data.get_self.source().inner_wise_self.source
+
+                yy = numpy.linspace(-5*self.source.Waist0/numpy.sqrt(2) + self.source.ZOrigin,
+                                    5*self.source.Waist0/numpy.sqrt(2) + self.source.ZOrigin,
+                                    self.number_of_points)
+
+                electric_fields = self.source.EvalField_XYSelf(numpy.zeros(self.number_of_points),
+                                                          yy)
+
+                source_wavefront = WiseWavefront(wavelength=self.source.Lambda,
+                                                 positions=yy/self.workspace_units_to_m,
+                                                 electric_fields=electric_fields,
+                                                 residuals=numpy.zeros(self.number_of_points))
+
+                self.send("GenericWavefront1D", source_wavefront.toGenericWavefront())
+            except Exception as exception:
+                QMessageBox.critical(self, "Error", str(exception), QMessageBox.Ok)
+
+                #raise exception
+        
 
     def set_input(self, input_data):
         self.setStatusMessage("")
@@ -60,21 +95,10 @@ class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
         if not input_data is None:
             try:
                 if input_data.has_source():
-                    source = input_data.get_source().inner_wise_source
-
-                    yy = numpy.linspace(-5*source.Waist0/numpy.sqrt(2) + source.ZOrigin,
-                                        5*source.Waist0/numpy.sqrt(2) + source.ZOrigin,
-                                        self.number_of_points)
-
-                    electric_fields = source.EvalField_XYSelf(numpy.zeros(self.number_of_points),
-                                                              yy)
-
-                    source_wavefront = WiseWavefront(wavelength=source.Lambda,
-                                                     positions=yy/self.workspace_units_to_m,
-                                                     electric_fields=electric_fields,
-                                                     residuals=numpy.zeros(self.number_of_points))
-
-                    self.send("GenericWavefront1D", source_wavefront.toGenericWavefront())
+                    self.source = input_data.get_source().inner_wise_source
+                    
+                    if self.is_automatic_execution:
+                        self.compute()
                 else:
                     raise ValueError("No source is present in input data")
             except Exception as exception:
--- orangecontrib/wise/widgets/wofry/ow_wise_wavefront_to_wofry.py
+++ orangecontrib/wise/widgets/wofry/ow_wise_wavefront_to_wofry.py
@@ -1,13 +1,13 @@
 import numpy
-from orangewidget import gui
-from oasys.widgets import gui as oasysgui
-
-from oasys.widgets import widget
 
 from PyQt5.QtWidgets import QApplication, QMessageBox
 from PyQt5.QtCore import QRect
 
 from orangewidget.settings import Setting
+from orangewidget import gui
+
+from oasys.widgets import gui as oasysgui
+from oasys.widgets.widget import AutomaticWidget
 
 from wofry.propagator.wavefront1D.generic_wavefront import GenericWavefront1D
 
@@ -16,7 +16,7 @@ from wofrywise.propagator.wavefront1D.wise_wavefront import WiseWavefront
 from orangecontrib.wise.util.wise_objects import WiseOutput, WiseNumericalIntegrationParameters
 from orangecontrib.wise.util.wise_propagator import WisePropagatorsChain, WisePropagationAlgorithms, WisePropagationParameters
 
-class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
+class OWWiseSourceToWofryWavefront1d(AutomaticWidget):
     name = "Wise Wavefront To Wofry Wavefront 1D"
     id = "toWofryWavefront1D"
     description = "Wise Wavefront To Wofry Wavefront 1D"
@@ -32,7 +32,9 @@ class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
                 "doc":"GenericWavefront1D",
                 "id":"GenericWavefront1D"}]
 
-    CONTROL_AREA_WIDTH = 405
+    MAX_WIDTH = 420
+    MAX_HEIGHT = 250
+    CONTROL_AREA_WIDTH = 410
 
     want_main_area = 0
 
@@ -46,25 +48,70 @@ class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
         geom = QApplication.desktop().availableGeometry()
         self.setGeometry(QRect(round(geom.width()*0.05),
                                round(geom.height()*0.05),
-                               round(min(geom.width()*0.98, self.CONTROL_AREA_WIDTH+15)),
-                               round(min(geom.height()*0.95, 125))))
+                               round(min(geom.width()*0.98, self.MAX_WIDTH)),
+                               round(min(geom.height()*0.95, self.MAX_HEIGHT))))
 
         self.setMinimumHeight(self.geometry().height())
         self.setMinimumWidth(self.geometry().width())
         self.setMaximumHeight(self.geometry().height())
         self.setMaximumWidth(self.geometry().width())
 
-        self.controlArea.setFixedWidth(self.CONTROL_AREA_WIDTH)
+        self.controlArea.setFixedWidth(self.MAX_WIDTH-10)
+        self.controlArea.setFixedHeight(self.MAX_HEIGHT-10)
 
-        main_box = oasysgui.widgetBox(self.controlArea, "WISE Wavefront to Wofry Wavefront Converter", orientation="vertical", width=self.CONTROL_AREA_WIDTH-5, height=100)
+        main_box = oasysgui.widgetBox(self.controlArea, "WISE Wavefront to Wofry Wavefront Converter", orientation="vertical", width=self.CONTROL_AREA_WIDTH-5, height=200)
 
         oasysgui.lineEdit(main_box, self, "area_size", "Area Size [" + u"\u03BC" + "m]", labelWidth=260, valueType=float, orientation="horizontal")
         oasysgui.lineEdit(main_box, self, "number_of_points", "Number of Points", labelWidth=260, valueType=int, orientation="horizontal")
         self.le_defocus_sweep = oasysgui.lineEdit(main_box, self, "defocus_sweep", "Defocus sweep", labelWidth=260, valueType=float, orientation="horizontal")
 
+        gui.button(main_box, self, "Compute", height=40, callback=self.compute)
+
+
     def after_change_workspace_units(self):
         label = self.le_defocus_sweep.parent().layout().itemAt(0).widget()
         label.setText(label.text() + " [" + self.workspace_units_label + "]")
+
+    def compute(self):
+        if not self.input_data is None:
+            try:
+                detector_size = self.area_size*1e-6
+                number_of_points = self.number_of_points
+    
+                previous_numerical_integration_parameters = self.input_data.get_numerical_integration_parameters()
+                numerical_integration_parameters = WiseNumericalIntegrationParameters(WiseNumericalIntegrationParameters.USER_DEFINED, detector_size, number_of_points)
+    
+                propagation_type = WisePropagationParameters.MIRROR_AND_DETECTOR
+    
+                #
+                # No need to recalculated wavefront on previous mirror surface
+                # if numerical integration parameters are identical
+                #
+                if WiseNumericalIntegrationParameters.USER_DEFINED == previous_numerical_integration_parameters.calculation_type:
+                    if self.number_of_points == previous_numerical_integration_parameters.number_of_points:
+                        propagation_type = WisePropagationParameters.DETECTOR_ONLY
+                        numerical_integration_parameters.calculated_number_of_points = previous_numerical_integration_parameters.calculated_number_of_points
+    
+                propagation_parameter = WisePropagationParameters(propagation_type=propagation_type,
+                                                                  source=self.input_data.get_source().inner_wise_source,
+                                                                  optical_element=self.input_data.get_optical_element().inner_wise_optical_element,
+                                                                  wavefront=self.input_data.get_wavefront(),
+                                                                  numerical_integration_parameters=numerical_integration_parameters,
+                                                                  defocus_sweep=self.defocus_sweep * self.workspace_units_to_m)
+    
+                propagation_output = WisePropagatorsChain.Instance().do_propagation(propagation_parameter,
+                                                                                    WisePropagationAlgorithms.HuygensIntegral)
+    
+                wavefront = WiseWavefront(wavelength=self.input_data.get_source().inner_wise_source.Lambda,
+                                          positions=propagation_output.det_s,
+                                          electric_fields=propagation_output.electric_fields,
+                                          residuals=numpy.zeros(self.number_of_points))
+    
+                self.send("GenericWavefront1D", wavefront.toGenericWavefront())
+            except Exception as exception:
+                QMessageBox.critical(self, "Error", str(exception), QMessageBox.Ok)
+
+                #raise exception
 
     def set_input(self, input_data):
         self.setStatusMessage("")
@@ -72,40 +119,11 @@ class OWWiseSourceToWofryWavefront1d(widget.OWWidget):
         if not input_data is None:
             try:
                 if input_data.has_wavefront():
+                    
+                    self.input_data = input_data
 
-                    detector_size = self.area_size*1e-6
-                    number_of_points = self.number_of_points
-
-                    previous_numerical_integration_parameters = input_data.get_numerical_integration_parameters()
-                    numerical_integration_parameters = WiseNumericalIntegrationParameters(WiseNumericalIntegrationParameters.USER_DEFINED, detector_size, number_of_points)
-
-                    propagation_type = WisePropagationParameters.MIRROR_AND_DETECTOR
-
-                    #
-                    # No need to recalculated wavefront on previous mirror surface
-                    # if numerical integration parameters are identical
-                    #
-                    if WiseNumericalIntegrationParameters.USER_DEFINED == previous_numerical_integration_parameters.calculation_type:
-                        if self.number_of_points == previous_numerical_integration_parameters.number_of_points:
-                            propagation_type = WisePropagationParameters.DETECTOR_ONLY
-                            numerical_integration_parameters.calculated_number_of_points = previous_numerical_integration_parameters.calculated_number_of_points
-
-                    propagation_parameter = WisePropagationParameters(propagation_type=propagation_type,
-                                                                      source=input_data.get_source().inner_wise_source,
-                                                                      optical_element=input_data.get_optical_element().inner_wise_optical_element,
-                                                                      wavefront=input_data.get_wavefront(),
-                                                                      numerical_integration_parameters=numerical_integration_parameters,
-                                                                      defocus_sweep=self.defocus_sweep * self.workspace_units_to_m)
-
-                    propagation_output = WisePropagatorsChain.Instance().do_propagation(propagation_parameter,
-                                                                                        WisePropagationAlgorithms.HuygensIntegral)
-
-                    wavefront = WiseWavefront(wavelength=input_data.get_source().inner_wise_source.Lambda,
-                                              positions=propagation_output.det_s,
-                                              electric_fields=propagation_output.electric_fields,
-                                              residuals=numpy.zeros(self.number_of_points))
-
-                    self.send("GenericWavefront1D", wavefront.toGenericWavefront())
+                    if self.is_automatic_execution:
+                        self.compute()
                 else:
                     raise ValueError("No wavefront is present in input data")
             except Exception as exception:
